020afe3aa2ed33e326e78f69491b338b578b3ad6
PWhiddy <all.cows.like.to.moo@gmail.com>
2022-05-29 14:56:30 -0400

    script version

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # sp-mosaic
+
+Script to create a mosiac of many images, visually sorted by the first 2 principal components of their CLIP embedding.
+
+Usage:
+```python
+from pathlib import Path
+import mosaic 
+
+path = Path("your/dir/with/imgs")
+all_paths = path.glob("*.png")           
+mosaic.create_mosaic(all_paths, 25, 512, 256, "test_grid.png")
+```
---
+++ mosaic.py
@@ -0,0 +1,52 @@
+import numpy as np
+import torch
+from sklearn.decomposition import PCA
+from PIL import Image
+from pathlib import Path
+from tqdm import tqdm
+import clip
+
+def create_mosaic(all_img_paths, dim, pre_crop, img_size, out_name):
+    model, preprocess = clip.load('ViT-B/32')
+    embeddings = torch.zeros(len(all_img_paths), 512)
+    print("Generating clip embeddings")
+    with torch.no_grad():
+        for idx, p in tqdm(list(enumerate(all_img_paths))):
+            img = preprocess(Image.open(p)).unsqueeze(0)
+            embeddings[idx] = model.encode_image(img).squeeze()
+    X = embeddings.numpy()
+    pca = PCA(n_components=2)
+    pca_vals = pca.fit_transform(X)
+    print(f"Explained variance by first 2 principal components: {pca.explained_variance_ratio_.sum()}")
+    comps_with_idx = [{"img_idx": idx, "comps": comps } for idx, comps in enumerate(pca_vals)]
+    print("Sorting grid")
+    # sort x 
+    x_sorted = sorted(comps_with_idx, key=lambda comps: comps["comps"][0])
+    grid_sorted = []
+    for i in range(dim):
+        row = x_sorted[i*dim:(i+1)*dim]
+        row_sorted = sorted(row, key=lambda comps: comps["comps"][1])
+        grid_sorted.append(row_sorted)
+    crop_amt = (pre_crop - img_size) / 2
+    total_dim = dim*img_size
+    main_img = np.zeros((total_dim,total_dim,4), dtype=np.uint8)
+    print("Creating Tiles")
+    for y_idx, row in tqdm(list(enumerate(grid_sorted))):
+        for x_idx, comps in enumerate(row):
+            img_idx = comps["img_idx"]
+            img = Image.open(all_img_paths[img_idx])
+            resized_img = img.resize((pre_crop, pre_crop))
+            if (resized_img.mode != "RGBA"):
+                # replace pixels matching alpha_val with transparency
+                new_img = np.ones((pre_crop,pre_crop,4), dtype=np.uint8)*255
+                new_img[:,:,:3] = resized_img
+                # from https://github.com/PWhiddy/PokemonRedExperiments/blob/master/MapWalkingVis.ipynb
+                alpha_val = np.array([255, 255,  255, 255], dtype=np.uint8)
+                alpha_mask = (new_img == alpha_val).all(axis=2).reshape(pre_crop,pre_crop,1)
+                resized_img = Image.fromarray( np.where(alpha_mask, np.array([[[0,0,0,0]]]), new_img).astype(np.uint8) )
+            cropped_img = resized_img.crop((crop_amt,crop_amt,pre_crop-crop_amt, pre_crop-crop_amt))
+            main_img[
+                x_idx*img_size:(x_idx+1)*img_size, 
+                y_idx*img_size:(y_idx+1)*img_size] = np.asarray(cropped_img)
+    im = Image.fromarray(main_img)
+    im.save(out_name)
